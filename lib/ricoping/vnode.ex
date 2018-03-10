d723269682f3e2684d934cde3567259876c32bd0
Enrique Fernandez <efcasado@gmail.com>
2018-03-10 11:23:50 +0100

    Add debug logs

--- lib/ricoping/vnode.ex
+++ lib/ricoping/vnode.ex
@@ -9,65 +9,82 @@ defmodule RicoPing.VNode do
   ##== Preamble ===========================================================
   @behaviour :riak_core_vnode
 
+  require Logger
+
 
   ##== VNode callbacks ====================================================
   def start_vnode(idx) do
+    Logger.debug "start_vnode/1"
     :riak_core_vnode_master.get_vnode_pid(idx, __MODULE__)
   end
 
   def init([partition]) do
+    Logger.debug "init/1"
     {:ok, %{:partition => partition}}
   end
 
   def handle_command(ping, _sender, state = %{:partition => partition}) do
+    Logger.debug "handle_command/3"
     res = {:pong, [{partition, node()}]}
     {:reply, res, state}
   end
   def handle_command(message, _sender, state) do
+    Logger.debug "handle_command/3"
     {:noreply, state}
   end
 
   def handle_handoff_command(_message, _sender, state) do
+    Logger.debug "handle_handoff_command/3"
     {:noreply, state}
   end
 
   def handoff_starting(_target_node, state) do
+    Logger.debug "handoff_starting/2"
     {true, state}
   end
 
   def handoff_cancelled(state) do
+    Logger.debug "handoff_cancelled/2"
     {:ok, state}
   end
 
   def handoff_finished(_target_node, state) do
+    Logger.debug "handoff_finished/2"
     {:ok, state}
   end
 
   def handle_handoff_data(_data, state) do
+    Logger.debug "handle_handoff_data/2"
     {:reply, :ok, state}
   end
 
   def encode_handoff_item(_objname, _objvalue) do
+    Logger.debug "encode_handoff_item/2"
     ""
   end
 
   def is_empty(state) do
+    Logger.debug "is_empty/1"
     {true, state}
   end
 
   def delete(state) do
+    Logger.debug "delete/1"
     {:ok, state}
   end
 
   def handle_coverage(_req, _key_spaces, _sender, state) do
+    Logger.debug "handle_coverage/4"
     {:stop, :not_implemented, state}
   end
 
   def handle_exit(_pid, _reason, state) do
+    Logger.debug "handle_exit/3"
     {:noreply, state}
   end
 
   def terminate(_reason, _state) do
+    Logger.debug "terminate/2"
     :ok
   end
 
