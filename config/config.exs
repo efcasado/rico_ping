759be8749bceaf1ff3b9c100069b1e528c12c28a
Enrique Fernandez <efcasado@gmail.com>
2018-03-10 13:04:31 +0100

    Update config

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,13 @@
 use Mix.Config
 
+config :lager,
+  error_logger_redirect: false,
+  error_logger_whitelist: [Logger.ErrorHandler]
+
+config :logger,
+  handle_sasl_reports: true,
+  handle_otp_reports: true
+
 config :rico_ping,
   http_port: 8080
 
@@ -7,12 +15,8 @@ config :riak_core,
   ring_state_dir: 'ring_data_dir',
   ring_creation_size: 64
 
-#config :riak_ensemble,
-  #synctree_backend: :synctree_ets,
-  #data_root: './data_root'
-
 config :kernel,
   inet_dist_listen_min: 9000,
-  inet_dist_listen_max: 9000
+  inet_dist_listen_max: 9100
 
 # import_config "#{Mix.env}.exs"
